6ae20a764a31ed7b5111ed8b40ed050fcf3e1a11
Phong <p7157568@gmail.com>
2016-03-18 23:09:23 +0700

    feat - Create Activity that has 2 button, DirectionActivity + ShareActivity.

---
+++ app/src/main/java/dhbk/android/httbdd_osm/DirectionActivity.java
@@ -0,0 +1,28 @@
+package dhbk.android.httbdd_osm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.osmdroid.api.IMapController;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+
+public class DirectionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_direction);
+
+        // Phong - show the map + add 2 zoom button + zoom at a default view point
+        MapView map = (MapView) findViewById(R.id.map);
+        map.setTileSource(TileSourceFactory.MAPNIK);
+        map.setBuiltInZoomControls(true);
+        map.setMultiTouchControls(true);
+        IMapController mapController = map.getController();
+        mapController.setZoom(18);
+        GeoPoint startPoint = new GeoPoint(10.772241, 106.657676);
+        mapController.setCenter(startPoint);
+    }
+}
--- app/src/main/java/dhbk/android/httbdd_osm/MainActivity.java
+++ app/src/main/java/dhbk/android/httbdd_osm/MainActivity.java
@@ -1,12 +1,9 @@
 package dhbk.android.httbdd_osm;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
-import org.osmdroid.api.IMapController;
-import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
-import org.osmdroid.util.GeoPoint;
-import org.osmdroid.views.MapView;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,15 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
-        // Phong - show the map + add 2 zoom button + zoom at a default view point
-        MapView map = (MapView) findViewById(R.id.map);
-        map.setTileSource(TileSourceFactory.MAPNIK);
-        map.setBuiltInZoomControls(true);
-        map.setMultiTouchControls(true);
-        IMapController mapController = map.getController();
-        mapController.setZoom(18);
-        GeoPoint startPoint = new GeoPoint(10.772241, 106.657676);
-        mapController.setCenter(startPoint);
+    public void onClickIntentDirection(View view) {
+        Intent intent = new Intent(this, DirectionActivity.class);
+        startActivity(intent);
+    }
+
+    public void onClickIntentShare(View view) {
+        Intent intent = new Intent(this, ShareActivity.class);
+        startActivity(intent);
     }
 }
