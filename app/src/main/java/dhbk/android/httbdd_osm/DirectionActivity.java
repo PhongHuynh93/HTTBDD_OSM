f30350f48a6f16b4b15268ab632dc2e9e0d99623
Phong <p7157568@gmail.com>
2016-03-24 16:26:43 +0700

    feat - make google client. WHen connect to google server, it receives the user's current location.

--- app/src/main/java/dhbk/android/httbdd_osm/DirectionActivity.java
+++ app/src/main/java/dhbk/android/httbdd_osm/DirectionActivity.java
@@ -1,14 +1,26 @@
 package dhbk.android.httbdd_osm;
 
+import android.location.Location;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 
 import org.osmdroid.api.IMapController;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
 
-public class DirectionActivity extends AppCompatActivity {
+public class DirectionActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+    // Phong
+    protected static final String TAG = DirectionActivity.class.getName();
+    protected GoogleApiClient mGoogleApiClient;
+    protected Location mLastLocation; // contain the first location when detect user's current location
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,5 +36,64 @@ public class DirectionActivity extends AppCompatActivity {
         mapController.setZoom(18);
         GeoPoint startPoint = new GeoPoint(10.772241, 106.657676);
         mapController.setCenter(startPoint);
+
+        // Phong - add GOogle Api to get the current location
+        buildGoogleApiClient();
+    }
+
+    // Phong - use Location service
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // Phong
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        // Phong
+        if (mGoogleApiClient.isConnected()) {
+            mGoogleApiClient.disconnect();
+        }
+    }
+
+    // Phong
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        // Provides a simple way of getting a device's location and is well suited for
+        // applications that do not require a fine-grained location and that do not need location
+        // updates. Gets the best and most recent location currently available, which may be null
+        // in rare cases when a location is not available.
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        if (mLastLocation != null) {
+            // TODO: 3/24/16 make a marker to this location
+        } else {
+            Toast.makeText(this, R.string.no_location_detected, Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+        // Refer to the javadoc for ConnectionResult to see what error codes might be returned in
+        // onConnectionFailed.
+        Log.i(TAG, "Connection failed: ConnectionResult.getErrorCode() = " + result.getErrorCode());
+    }
+
+
+    @Override
+    public void onConnectionSuspended(int cause) {
+        // The connection to Google Play services was lost for some reason. We call connect() to
+        // attempt to re-establish the connection.
+        Log.i(TAG, "Connection suspended");
+        mGoogleApiClient.connect();
     }
 }
